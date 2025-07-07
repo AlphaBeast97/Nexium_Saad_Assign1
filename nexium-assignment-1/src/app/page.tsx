e114c734a9b9a588cbbd4fd8df7d2b28c9ef18ba
AlphaBeast97 <ksaad5272@gmail.com>
2025-07-07 13:27:29 +0500

    style: added a favicon

--- nexium-assignment-1/src/app/page.tsx
+++ nexium-assignment-1/src/app/page.tsx
@@ -4,10 +4,11 @@ export const dynamic = "force-static";
 export const metadata = {
   title: "Quote Generator",
   description: "A simple quote generator application",
+  icons: {
+    icon: "/quote.png",
+  },
 };
 
 export default function Home() {
-  return (
-    <Hero />
-  );
+  return <Hero />;
 }
